059aa9218fadff7c8d2383b47cb3092d5f63d4e7
Luke Watts <luke@thisis.la>
2025-04-01 14:01:59 +0200

    refactor: improve code structure and remove mixed concerns

--- scripts/next_date.go
+++ scripts/next_date.go
@@ -9,42 +9,55 @@ import (
 	"github.com/dustin/go-humanize"
 )
 
+const (
+	datetimePattern = `<time datetime="[0-9]{4}-[0-9]{2}-[0-9]{2}">[\s0-9A-Za-z]+</time>`
+)
+
 func getNextMeetupDate(now time.Time) time.Time {
-	next := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
+	nextMeetup := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
 
 	// Loop until you find a Thursday
-	for next.Weekday() != time.Thursday {
-		next = next.AddDate(0, 0, 1)
+	for nextMeetup.Weekday() != time.Thursday {
+		nextMeetup = nextMeetup.AddDate(0, 0, 1)
 	}
 
-	return next.AddDate(0, 0, 14)
+	return nextMeetup.AddDate(0, 0, 14)
+}
+
+func updateHtmlWithNewDate(targetFile string, newDate time.Time) error {
+	data, err := os.ReadFile(targetFile)
+	if err != nil {
+		return fmt.Errorf("error reading file: %w", err)
+	}
+
+	newEventStr := fmt.Sprintf("<time datetime=\"%s\">%s</time>",
+		fmt.Sprintf("%v-%02d-%v", newDate.Year(), int(newDate.Month()), newDate.Day()),
+		fmt.Sprintf("%s %s %d", humanize.Ordinal(newDate.Day()), newDate.Month(), newDate.Year()),
+	)
+
+	re := regexp.MustCompile(datetimePattern)
+	str := re.ReplaceAllString(string(data), newEventStr)
+
+	err = os.WriteFile(targetFile, []byte(str), 0644)
+
+	return err
 }
 
 // Print the date for the next meetup
 // which will be the 3rd Thursday of the next month
 func main() {
-	fmt.Println("Day of the next meetup is:", getNextMeetupDate(time.Now()))
-
-	// Update index.html with the date
-	data, err := os.ReadFile("../index.html")
-	if err != nil {
-		panic(err)
-	}
+	targetFile := "../index.html"
 
 	nextEventDate := getNextMeetupDate(time.Now())
 
-	newEventStr := fmt.Sprintf("<time datetime=\"%s\">%s</time>",
-		fmt.Sprintf("%v-%02d-%v", nextEventDate.Year(), int(nextEventDate.Month()), nextEventDate.Day()),
-		fmt.Sprintf("%s %s %d", humanize.Ordinal(nextEventDate.Day()), nextEventDate.Month(), nextEventDate.Year()),
-	)
+	fmt.Println("Day of the next meetup is:", nextEventDate.Format(time.RFC822))
 
-	// fmt.Sprintf("%s %s", humanize.Ordinal(next.Day()), next.Month())
-	re := regexp.MustCompile(`<time datetime="[0-9]{4}-[0-9]{2}-[0-9]{2}">[\s0-9A-Za-z]+</time>`)
-	str := re.ReplaceAllString(string(data), newEventStr)
+	err := updateHtmlWithNewDate(targetFile, nextEventDate)
 
-	fmt.Println("Writing to index.html")
-	err = os.WriteFile("../index.html", []byte(str), 0644)
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Error writing file: %v\n", err)
+		os.Exit(1)
 	}
+
+	fmt.Println("Successfully updated date in index.html")
 }
